a12b6509b192b63a5242001a09bac8287f93454c
Yan <203676187@qq.com>
2020-08-09 20:34:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chatting/MsgAdapter.java
@@ -0,0 +1,69 @@
+package com.example.chatting;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
+    private List<Msg> mMsgList;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.msg_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Msg msg=mMsgList.get(position);
+        if(msg.getType()==Msg.TYPE_RECEIVED)
+        {
+            holder.leftLayout.setVisibility(View.VISIBLE);
+            holder.rightLayout.setVisibility(View.GONE);
+            holder.leftMsg.setText(msg.getContent());
+        }
+        else if (msg.getType()==Msg.TYPE_SENT)
+        {
+            holder.rightLayout.setVisibility(View.VISIBLE);
+            holder.leftLayout.setVisibility(View.GONE);
+            holder.rightMsg.setText(msg.getContent());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMsgList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder
+    {
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+
+        TextView leftMsg;
+        TextView rightMsg;
+
+        public ViewHolder(View view) {
+            super(view);
+            this.leftLayout = (LinearLayout) view.findViewById(R.id.left_layout);
+            this.rightLayout = (LinearLayout) view.findViewById(R.id.right_layout);
+            this.leftMsg = (TextView) view.findViewById(R.id.left_msg);
+            this.rightMsg = (TextView) view.findViewById(R.id.right_msg);
+        }
+    }
+    public MsgAdapter(List<Msg> msgList)
+    {
+        mMsgList=msgList;
+    }
+
+
+
+}
